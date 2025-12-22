4b7a9e8d8b4290f16ced06bb2c0d27ea3d571d76
Adrian Suliga <adriansuliga81@gmail.com>
2025-12-22 20:12:09 +0100

    Solve AoC 2024 Day 4 in C

--- C/utils/data_parsing/file_read.h
+++ C/utils/data_parsing/file_read.h
@@ -9,6 +9,9 @@ int get_file_char_count(const char *file_name);
 // Count number of lines in file with given file name
 int get_file_line_count(const char *file_name);
 
+// Count number of chars in first line of a file
+int get_file_first_line_width(const char *file_name);
+
 // Count maximum number of times sign appears in one line
 int get_file_max_char_count(const char *file_name, const char sign);
 
@@ -26,6 +29,9 @@ void read_file_with_2_columns(const char *file_name, const char *separator,
 // present in a row
 void read_file_with_n_numbers_per_line(const char *file_name, const char *separator,
                                     int **output, int size, int k);
+
+// Read strings from file with n lines and k columns
+void read_2d_char_array(const char *file_name, char **output, int n, int k);
 
 // Scan file for given regex, save maximum of size to output
 void scan_file_for_regex(const char *file_name, const regex_t *regex,
